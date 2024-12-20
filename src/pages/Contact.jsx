cab8fd10ad8d8030c05afd9afe236c96ecf78bb8
Mujahid087 <mujahidmahedi10@gmail.com>
2024-12-20 21:08:23 +0530

    added the external links

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,103 @@
+// import React from "react";
+// import { Link } from "react-router-dom";
+// import { Container, Row, Col, Form, FormGroup, Input } from "reactstrap";
+// import Helmet from "../components/Helmet/Helmet";
+// import CommonSection from "../components/UI/CommonSection";
+
+// import "../styles/contact.css";
+
+
+// const socialLinks = [
+//   {
+//     url: "https://www.facebook.com/",
+//     icon: "ri-facebook-line",
+//   },
+//   {
+//     url: "https://www.instagram.com/",
+//     icon: "ri-instagram-line",
+//   },
+//   {
+//     url: "https://www.linkedin.com/",
+//     icon: "ri-linkedin-line",
+//   },
+//   {
+//     url: "https://twitter.com/",
+//     icon: "ri-twitter-line",
+//   },
+// ];
+
+
+// const Contact = () => {
+//   return (
+//     <Helmet title="Contact">
+//       <CommonSection title="Contact" />
+//       <section>
+//         <Container>
+//           <Row>
+//             <Col lg="7" md="7">
+//               <h6 className="fw-bold mb-4">Get In Touch</h6>
+
+//               <Form>
+//                 <FormGroup className="contact__form">
+//                   <Input placeholder="Your Name" type="text" />
+//                 </FormGroup>
+//                 <FormGroup className="contact__form">
+//                   <Input placeholder="Email" type="email" />
+//                 </FormGroup>
+//                 <FormGroup className="contact__form">
+//                   <textarea
+//                     rows="5"
+//                     placeholder="Message"
+//                     className="textarea"
+//                   ></textarea>
+//                 </FormGroup>
+
+//                 <button className=" contact__btn" type="submit">
+//                   Send Message
+//                 </button>
+//               </Form>
+//             </Col>
+
+//             <Col lg="5" md="5">
+//               <div className="contact__info">
+//                 <h6 className="fw-bold">Contact Information</h6>
+//                 <p className="section__description mb-0">
+//                   123 ZindaBazar, Sylhet, Bangladesh
+//                 </p>
+//                 <div className=" d-flex align-items-center gap-2">
+//                   <h6 className="fs-6 mb-0">Phone:</h6>
+//                   <p className="section__description mb-0">+88683896366</p>
+//                 </div>
+
+//                 <div className=" d-flex align-items-center gap-2">
+//                   <h6 className="mb-0 fs-6">Email:</h6>
+//                   <p className="section__description mb-0">example@gmail.com</p>
+//                 </div>
+
+//                 <h6 className="fw-bold mt-4">Follow Us</h6>
+
+//                 <div className=" d-flex align-items-center gap-4 mt-3">
+//                   {socialLinks.map((item, index) => (
+//                     <Link
+//                       to={item.url}
+//                       key={index}
+//                       className="social__link-icon"
+//                     >
+//                       <i class={item.icon}></i>
+//                     </Link>
+//                   ))}
+//                 </div>
+//               </div>
+//             </Col>
+//           </Row>
+//         </Container>
+//       </section>
+//     </Helmet>
+//   );
+// };
+
+// export default Contact;
 import React from "react";
-import { Link } from "react-router-dom";
 import { Container, Row, Col, Form, FormGroup, Input } from "reactstrap";
 import Helmet from "../components/Helmet/Helmet";
 import CommonSection from "../components/UI/CommonSection";
@@ -8,19 +106,19 @@ import "../styles/contact.css";
 
 const socialLinks = [
   {
-    url: "#",
+    url: "https://www.facebook.com/",
     icon: "ri-facebook-line",
   },
   {
-    url: "#",
+    url: "https://www.instagram.com/",
     icon: "ri-instagram-line",
   },
   {
-    url: "#",
+    url: "https://www.linkedin.com/",
     icon: "ri-linkedin-line",
   },
   {
-    url: "#",
+    url: "https://twitter.com/",
     icon: "ri-twitter-line",
   },
 ];
@@ -50,7 +148,7 @@ const Contact = () => {
                   ></textarea>
                 </FormGroup>
 
-                <button className=" contact__btn" type="submit">
+                <button className="contact__btn" type="submit">
                   Send Message
                 </button>
               </Form>
@@ -62,27 +160,29 @@ const Contact = () => {
                 <p className="section__description mb-0">
                   123 ZindaBazar, Sylhet, Bangladesh
                 </p>
-                <div className=" d-flex align-items-center gap-2">
+                <div className="d-flex align-items-center gap-2">
                   <h6 className="fs-6 mb-0">Phone:</h6>
                   <p className="section__description mb-0">+88683896366</p>
                 </div>
 
-                <div className=" d-flex align-items-center gap-2">
+                <div className="d-flex align-items-center gap-2">
                   <h6 className="mb-0 fs-6">Email:</h6>
                   <p className="section__description mb-0">example@gmail.com</p>
                 </div>
 
                 <h6 className="fw-bold mt-4">Follow Us</h6>
 
-                <div className=" d-flex align-items-center gap-4 mt-3">
+                <div className="d-flex align-items-center gap-4 mt-3">
                   {socialLinks.map((item, index) => (
-                    <Link
-                      to={item.url}
+                    <a
+                      href={item.url}
                       key={index}
                       className="social__link-icon"
+                      target="_blank"
+                      rel="noopener noreferrer"
                     >
-                      <i class={item.icon}></i>
-                    </Link>
+                      <i className={item.icon}></i>
+                    </a>
                   ))}
                 </div>
               </div>
